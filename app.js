2f4630b8303250f587342a2058b7cc171276c22b
shariharan182003 <shariharan182003@gmail.com>
2024-01-14 13:53:39 +0530

    docker changes

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ app.use(express.json());
 
 const python_router = require("./routes/python_route");
 app.use("/api/python", python_router);
+app.get("/health", (req, res) => {
+  return res.status(200).json({ message: "health check" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}/`);
